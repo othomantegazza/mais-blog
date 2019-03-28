9b06e966b6639016be225d8d762357d6601c30d9
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-03-28 21:57:48 +0100

    try merge production and shape

--- counties-italy.R
+++ counties-italy.R
@@ -24,19 +24,52 @@ if(!file.exists(municip_path)) {
   
 }
 
-pro_ita <- 
+prov_ita <- 
   paste0("data/istat_municipalities/Limiti_2016_WGS84_g/",
          "CMProv2016_WGS84_g/CMprov2016_WGS84_g.shp") %>% 
-  sf::st_read()
+  sf::st_read() %>% 
+  janitor::clean_names()
 
 
 # plot shapes -------------------------------------------------------------
 
 library(ggspatial)
 
-pro_ita %>%
+prov_ita %>%
   ggplot() +
   geom_sf() +
   # scale_fill_viridis_c(trans = "log") +
   # theme_bw()
   theme_minimal()
+
+
+# load production istat ---------------------------------------------------
+
+# generated in get-prod-istat.R
+load("data/mais-prod-istat.Rdata") 
+
+
+production_tidy <- 
+  mais_istat %>% 
+  # three methods: "r" "t" "s", why?
+  # looks like t are regions
+  filter(metodo != "t",
+         provincia != "ITALIA")
+
+# merge by "provincia
+# no_match <- 
+#   production_tidy %>% 
+#   filter(! provincia %in% prov_ita$provincia) %>% 
+#   pull(provincia)
+# 
+# no_match2 <- 
+#   prov_ita %>% 
+#   filter(! provincia %in% production_tidy$provincia) %>% 
+#   pull(provincia)
+
+prov
+
+production_tidy$provincia %in% prov_ita$provincia
+prov_ita$provincia %in% production_tidy$provincia 
+
+
---
+++ get-prod-istat.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(readxl)
+
+
+
+# Download data -----------------------------------------------------------
+
+istat_path <- "data/mais-prod-istat.Rdata"
+
+#  they do not provide https
+istat_url <- "http://agri.istat.it/excel/dwplC02aa00000100000130.xls"
+
+# http://agri.istat.it/excel/dwplC02aa00000100000130.xls
+
+
+if(!file.exists(istat_path)) {
+ keep <- "_" %>% paste0(6:9) %>%   paste(collapse = "|")
+
+ mais_istat <- 
+   read_excel(istat_url,
+              skip = 2) %>% 
+   janitor::clean_names() %>% 
+   select(x1, matches(keep)) %>% 
+   rename(provincia = "x1") %>% 
+   rename_all(~str_remove_all(., "_[6789]"))
+ 
+ save(mais_istat, file = istat_path)
+} else {
+  load(istat_path)
+}
+
